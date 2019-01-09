b38f59a755e9f3ff33deb80b45c109528988e1e1
LeoMosiah <leonardomosiah@gmail.com>
2019-01-09 14:31:17 -0200

    navegacao

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+import { DecksScreen } from './screens/DecksScreen';
+import { NewDeckScreen } from './screens/NewDeckScreen';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+export default function App() {
+  return (
+    <AppContainer/>
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const AppNavigator = createBottomTabNavigator(
+  {
+    'Decks': { screen: DecksScreen },
+    'New Deck': { screen: NewDeckScreen },
+  }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
