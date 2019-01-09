cc02850e13d6ba66a98708c7a6d87c4b32410147
LeoMosiah <leonardomosiah@gmail.com>
2019-01-09 14:28:45 -0200

    começando navegacao

---
+++ screens/DecksScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from "react-native";
+
+export const DecksScreen = () =>  (
+    <View>
+      <Text>
+        Decks Screen
+      </Text>
+    </View>
+  );
---
+++ screens/NewDeckScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from "react-native";
+
+export const NewDeckScreen = () => (
+    <View>
+      <Text>
+        New Deck Screen
+      </Text>
+    </View>
+  );
