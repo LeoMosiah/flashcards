17955a5b1d3ec9c1b4fc7697400a66112b087789
LeoMosiah <leonardomosiah@gmail.com>
2019-01-09 15:54:48 -0200

    footer

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createBottomTabNavigator, createAppContainer } from "react-navigation";
 import { DecksScreen } from './screens/DecksScreen';
 import { NewDeckScreen } from './screens/NewDeckScreen';
+import { FontAwesome, MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function App() {
   return (
@@ -13,7 +14,24 @@ const AppNavigator = createBottomTabNavigator(
   {
     'Decks': { screen: DecksScreen },
     'New Deck': { screen: NewDeckScreen },
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ tintColor }) => {
+        const { routeName } = navigation.state;
+        return routeName === 'Decks' ? (
+          <MaterialCommunityIcons name="cards-outline" size={30} color={tintColor} />
+        ) : (
+          <FontAwesome name="plus-square" size={30} color={tintColor} />
+        );
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    },
   }
+
 );
 
 const AppContainer = createAppContainer(AppNavigator);
