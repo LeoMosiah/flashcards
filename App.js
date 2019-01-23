3e230c51c827fcd829cc6a3a963662e1033a8b89
LeoMosiah <leonardomosiah@gmail.com>
2019-01-23 07:51:31 -0200

    removing navigation

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { DecksScreen } from './screens/DecksScreen';
+
+export default class App extends Component {
+  render() {
+    return (
+      <DecksScreen/>
+    );
+  }
+}
+
+
